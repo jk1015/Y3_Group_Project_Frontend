c83a7ea948769b1ce7bb0df2117851c1034765d0
ak8815 <ak8815@ic.ac.uk>
2018-10-19 03:03:30 +0100

    Implemented the header and added HTMLtoReactparser.

--- src/Student.js
+++ src/Student.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
 
 import { askQuestion } from './api';
+//var student_page = require('./student.html.js');
+var HtmlToReactParser = require('html-to-react').Parser;
+var htmlToReactParser = new HtmlToReactParser();
+var html_page =
+      '<script src="/socket.io/socket.io.js"></script>' +
+      '<script>' +
+        'var socket = io();' +
+        'socket.on("question received", function(res){' +
+           'printResult(res.question)' +
+        '});' +
+        'function printResult(message) {' +
+          'document.getElementById("res").innerHTML = message;' +
+        '}' +
+        'function ask() {' +
+          'socket.emit("question asked", "just a // QUESTION: ")' +
+        '}' +
+      '</script>' +
+      '<div>' +
+        '<p>Student Haya</p>' +
+        '<p id="res"></p>' +
+        '<button onclick="this.ask()">Ask</button>' +
+        '<script>' +
+          'ask();' +
+        '</script>' +
+      '</div>';
+var header =
+  '<div id="header">' +
+    '<p>Student Section</p>' +
+  '</div>';
 
+function Header() {
+  return (
+    <div id="header">
+      <p>Student Room</p>
+    </div>
+  );
+}
+
+var jsx_page = htmlToReactParser.parse(html_page);
+var jsx_header = htmlToReactParser.parse(header);
 class Student extends Component {
 
   constructor(props) {
@@ -19,10 +58,7 @@ class Student extends Component {
 
   render() {
     return (
-          <div>
-          <p>Student</p>
-          <button onClick={() => this.ask()}>Ask</button>
-          </div>
+      <Header />
     );
   }
 }
