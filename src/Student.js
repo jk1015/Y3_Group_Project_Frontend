d657dbd4f31cfbe51493fa81622abcfa89e19a73
docuser <jk1015@ic.ac.uk>
2018-10-19 14:11:47 +0100

    Added default question buttons

--- src/Student.js
+++ src/Student.js
@@ -92,9 +92,26 @@ class Student extends Component {
           </form>
           <button onClick={this.ask}>ASK</button>
         </div>
+        <div id="Understand">
+          <h2>I DON'T UNDERSTAND</h2>
+          <button onClick={askQuestion("I don't understand")}>ASK</button>
+        </div>
+        <div id="Example">
+          <h2>Could you give an example?</h2>
+          <button onClick={askQuestion("Could you give an example?")}>ASK</button>
+        </div>
+        <div id="Slower">
+          <h2>Could you slow down?</h2>
+          <button onClick={askQuestion("Could you slow down?")}>ASK</button>
+        </div>
+        <div id="Faster">
+          <h2>Could you speed up?</h2>
+          <button onClick={askQuestion("Could you speed up?")}>ASK</button>
+        </div>
       </div>
     );
   }
 }
+
 
 export default Student;
