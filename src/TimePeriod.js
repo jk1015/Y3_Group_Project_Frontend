df8efadcf77478fa6d1022a0aab73fc5883c2db3
Max Smith <mws15@ic.ac.uk>
2019-01-11 16:24:50 +0000

    Refactored part of TimePeriod to remove duplicate code

--- src/TimePeriod.js
+++ src/TimePeriod.js
@@ -9,6 +9,17 @@ function redirectTo(url, logout) {
   window.location.href = url;
 }
 
+function returnOptionList(startno, endno) {
+  let nums = [];
+  for (let i = startno; i <= endno; i++) {
+    nums.push(i);
+  }
+  let list = nums.map((num)=>{
+    return <option key={num} value={num}>{num}</option>
+  });
+  return list;
+}
+
 class TimePeriod extends React.Component {
 
   constructor(props) {
@@ -69,96 +80,25 @@ class TimePeriod extends React.Component {
           <div className="form-group">
           <label htmlFor="month">month</label>
           <select className="form-control" id="month" defaultValue={this.state.month} onChange={this.updateMonth.bind(this)}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="7">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
+            {returnOptionList(1, 12)}
           </select>
           </div>
           <div className="form-group">
           <label htmlFor="day">day</label>
           <select className="form-control" id="day" defaultValue={this.state.day} onChange={this.updateDay.bind(this)}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
-            <option value="24">24</option>
-            <option value="25">25</option>
-            <option value="26">26</option>
-            <option value="27">27</option>
-            <option value="28">28</option>
-            <option value="29">29</option>
-            <option value="30">30</option>
-            <option value="31">31</option>
+            {returnOptionList(1, 31)}
           </select>
           </div>
           <div className="form-group">
           <label htmlFor="begin">hour at begin</label>
           <select className="form-control" id="begin" defaultValue={this.state.begin_hour} onChange={this.updateBegin.bind(this)}>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
+            {returnOptionList(7, 22)}
           </select>
           </div>
           <div className="form-group">
           <label htmlFor="end">hour at end</label>
           <select className="form-control" id="end" defaultValue={this.state.end_hour} onChange={this.updateEnd.bind(this)}>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
+            {returnOptionList(8, 23)}
           </select>
           </div>
           <div className="input-group-append mt-4">
